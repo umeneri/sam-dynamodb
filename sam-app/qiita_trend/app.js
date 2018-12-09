fdb7a3d354d1560e46956beb97ddd7b63c571d50
umeneri <koishimst@gmail.com>
2018-12-09 21:10:10 +0900

    create local chrome screenshot handler

--- sam-app/qiita_trend/app.js
+++ sam-app/qiita_trend/app.js
@@ -11,58 +11,64 @@ exports.lambdaHandler = async (event, context, callback) => {
 
   try {
     slsChrome = await launchChrome()
+    console.log('qiita chrome');
     browser = await puppeteer.connect({
       browserWSEndpoint: (await CDP.Version()).webSocketDebuggerUrl
     })
+    console.log('qiita browser');
     const context = browser.defaultBrowserContext()
-
     const page = await context.newPage()
-    // await page.setExtraHTTPHeaders({ 'Accept-Language': 'ja' })
-    // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36')
-    // await page.goto(`https://www.google.co.jp/`, { waitUntil: 'networkidle0' })
-    //
-    // const searchWord = encodeURIComponent(event.searchWord)
-    // await page.goto(`https://www.google.co.jp/search?q=${searchWord}`, { waitUntil: 'domcontentloaded' })
-    //
-    // await page.evaluate(() => {
-    //     var style = document.createElement('style')
-    //     style.textContent = `
-    //         @import url('//fonts.googleapis.com/css?family=Source+Code+Pro');
-    //         @import url('//fonts.googleapis.com/earlyaccess/notosansjp.css');
-    //         div, input, a{ font-family: 'Noto Sans JP', sans-serif !important; };`
-    //     document.head.appendChild(style)
-    // })
-    // await page.waitFor(1000) // Wait until the font is reflected
-    // const screenShot = await page.screenshot({fullPage: true})
-    // const searchResults = await page.evaluate(() => {
-    //   const ret = []
-    //   const nodeList = document.querySelectorAll("div#search h3")
-    //
-    //   nodeList.forEach(node => {
-    //     ret.push(node.innerText)
-    //   })
-    //
-    //   return ret
-    // })
-    //
-    // await page.waitFor(1000)
-    // return callback(null, {result: 'OK', searchResults: searchResults, screenShot: screenShot.toString('hex')})
-    return callback(null, {result: 'OK', searchResults: 'result', screenShot: 'shot'})
+    console.log('qiita page');
+
+    await page.setExtraHTTPHeaders({ 'Accept-Language': 'ja' })
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36')
+    await page.goto(`https://www.google.co.jp/`, { waitUntil: 'networkidle0' })
+
+    const searchWord = encodeURIComponent(event.searchWord)
+    await page.goto(`https://www.google.co.jp/search?q=${searchWord}`, { waitUntil: 'domcontentloaded' })
+
+    await page.evaluate(() => {
+        var style = document.createElement('style')
+        style.textContent = `
+            @import url('//fonts.googleapis.com/css?family=Source+Code+Pro');
+            @import url('//fonts.googleapis.com/earlyaccess/notosansjp.css');
+            div, input, a{ font-family: 'Noto Sans JP', sans-serif !important; };`
+        document.head.appendChild(style)
+    })
+    await page.waitFor(1000) // Wait until the font is reflected
+    const screenShot = await page.screenshot({fullPage: true})
+    const searchResults = await page.evaluate(() => {
+      const ret = []
+      const nodeList = document.querySelectorAll("div#search h3")
+
+      nodeList.forEach(node => {
+        ret.push(node.innerText)
+      })
+
+      return ret
+    })
+
+    await page.waitFor(1000)
+
+    const result = {result: 'OK', searchResults: searchResults, screenShot: screenShot.toString('hex').length };
+    console.log(result);
+    return callback(null, result);
+    // return callback(null, {result: 'OK', searchResults: 'result', screenShot: 'shot'})
   } catch (err) {
     console.error(err)
     return callback(null, JSON.stringify({ result: 'NG' }))
   } finally {
-    // if (page) {
-    //   await page.close()
-    // }
-    //
-    // if (browser) {
-    //   await browser.disconnect()
-    // }
-    //
-    // if (slsChrome) {
-    //   await slsChrome.kill()
-    // }
+    if (page) {
+      await page.close()
+    }
+
+    if (browser) {
+      await browser.disconnect()
+    }
+
+    if (slsChrome) {
+      await slsChrome.kill()
+    }
   }
 }
 
